2791a9054689c9ef250bf9d581bf68a79dff2ab3
Vinicius Pacheco <vin175pacheco@gmail.com>
2022-04-18 11:23:37 -0300

    test: push coverage to 100%

--- test/evaluateClassName.test.tsx
+++ test/evaluateClassName.test.tsx
@@ -1,81 +1,135 @@
 import { evaluateClassName } from '../src/utils';
 
 describe('evaluateClassName', () => {
+  const size = {
+    sm: 'z',
+    xl: 'q',
+  };
   const defaultClassName = 'm';
   const variants = {
     color: {
       gray: 'x',
       red: 'y',
     },
-    size: {
-      sm: 'z',
-      xl: 'q',
-    },
+    size: (value: 'sm' | 'xl') => size[value],
   } as const;
 
+  describe('when nothing matches and no defaultClassName is given', () => {
+    it('should return an empty string', () => {
+      expect(evaluateClassName({}, {})).toEqual('');
+    });
+  });
+
   describe.each([
-    ['no props match', 'default class name', {}, [defaultClassName]],
+    ['no props match', 'default class name', {}, {}, [defaultClassName]],
     [
       'no props match',
       'default class name',
       { color: '_' },
+      undefined,
       [defaultClassName],
     ],
     [
       'no props match',
       'default class name',
       { color: '_', size: '_' },
+      {},
       [defaultClassName],
     ],
     [
       'no props match',
       'default class name',
       { size: 'gray' },
+      {},
       [defaultClassName],
+    ],
+    [
+      'no props match but has a default prop value',
+      'default class name',
+      { color: '_' },
+      { size: 'sm' },
+      [defaultClassName, size.sm],
     ],
     [
       'no props match',
       'default class name',
       { color: 'sm' },
+      {},
       [defaultClassName],
     ],
     [
       '1 prop match',
       'color.gray className',
       { color: 'gray' },
+      {},
       [variants.color.gray, defaultClassName],
+    ],
+    [
+      '1 prop match and has a default prop value',
+      'color.gray and size.sm className',
+      { color: 'gray' },
+      { size: 'sm' },
+      [variants.color.gray, size.sm, defaultClassName],
     ],
     [
       '1 prop match',
       'color.red className',
       { color: 'red' },
+      {},
       [variants.color.red, defaultClassName],
     ],
     [
       '1 prop match',
       'size.sm className',
       { size: 'sm' },
-      [variants.size.sm, defaultClassName],
+      {},
+      [size.sm, defaultClassName],
     ],
     [
       '2 props match',
       'color.gray and size.sm className',
       { color: 'gray', size: 'sm' },
-      [variants.color.gray, variants.size.sm, defaultClassName],
+      {},
+      [variants.color.gray, size.sm, defaultClassName],
     ],
     [
       '2 props match',
       'color.red and size.xl className',
       { color: 'red', size: 'xl' },
-      [variants.color.red, variants.size.xl, defaultClassName],
+      {},
+      [variants.color.red, size.xl, defaultClassName],
     ],
-  ])('when %s', (_, expectedResultMessage, props, expectedClassNames) => {
-    it(`should render return ${expectedResultMessage}`, () => {
-      expect(
-        evaluateClassName(props, variants, defaultClassName)
-          .split(' ')
-          .sort()
-      ).toEqual(expectedClassNames.sort());
-    });
-  });
+    [
+      'has a default prop value and undefined is declared',
+      'size.sm className',
+      { size: undefined },
+      { size: 'sm' },
+      [size.sm, defaultClassName],
+    ],
+    [
+      'prop.className is passed',
+      'defaultClassName and prop.className',
+      { className: 'customprop' },
+      {},
+      ['customprop', defaultClassName],
+    ],
+    [
+      'prop.className is passed and variants are given',
+      'defaultClassName and prop.className',
+      { color: 'gray', size: 'sm', className: 'customprop' },
+      {},
+      ['customprop', variants.color.gray, size.sm, defaultClassName],
+    ],
+  ])(
+    'when %s',
+    (_, expectedResultMessage, props, defaultVariants, expectedClassNames) => {
+      it(`should return ${expectedResultMessage}`, () => {
+        expect(
+          evaluateClassName(props, variants, defaultVariants, defaultClassName)
+            .split(' ')
+            .sort()
+        ).toEqual(expectedClassNames.sort());
+      });
+    }
+  );
 });
---
+++ test/styled.test.tsx
@@ -0,0 +1,251 @@
+import { w, W } from '../src';
+import { expectType } from 'tsd';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+describe('styled', () => {
+  const defaultClassName = 'xyz';
+
+  describe('when using a tag', () => {
+    const variants = {
+      color: {
+        gray: 'x',
+        red: 'y',
+      },
+      size: {
+        sm: 'z',
+        xl: 'q',
+      },
+    } as const;
+    const Button = w.button(defaultClassName, {
+      variants,
+      defaultProps: {
+        size: 'sm',
+      },
+    });
+
+    it('should render without errors', () => {
+      render(<Button color="gray" />);
+      expect(screen.getByRole('button')).toBeInTheDocument();
+    });
+
+    describe('if no classnames are evaluated', () => {
+      const RawButton = w.button('');
+
+      it('should render without errors', () => {
+        render(<RawButton />);
+        expect(screen.getByRole('button')).toBeInTheDocument();
+        expect(screen.getByRole('button')).not.toHaveAttribute('class');
+      });
+    });
+
+    describe('if not declaring variants', () => {
+      const RawButton = w.button(defaultClassName);
+
+      it('should render without errors', () => {
+        render(<RawButton />);
+        expect(screen.getByRole('button')).toBeInTheDocument();
+        expect(screen.getByRole('button')).toHaveAttribute(
+          'class',
+          defaultClassName
+        );
+      });
+    });
+
+    describe.each([
+      [
+        'color=gray',
+        { color: 'gray' },
+        [defaultClassName, variants.color.gray, variants.size.sm],
+      ],
+      [
+        'color=gray size=sm',
+        { color: 'gray', size: 'sm' },
+        [defaultClassName, variants.color.gray, variants.size.sm],
+      ],
+      [
+        'color=gray size=undefined',
+        { color: 'gray', size: undefined },
+        [defaultClassName, variants.color.gray, variants.size.sm],
+      ],
+      [
+        'color=red size=xl',
+        { color: 'red', size: 'xl' },
+        [defaultClassName, variants.color.red, variants.size.xl],
+      ],
+    ])('if rendering with props %s', (_, props, expectedClassNames) => {
+      it('should pass the evaluated className prop', () => {
+        render(<Button {...(props as any)} />);
+        const element = screen.getByRole('button');
+        expect(element.className.split(' ').sort()).toEqual(
+          expectedClassNames.sort()
+        );
+      });
+    });
+
+    it('should set correct prop types inferred by variants', () => {
+      expectType<W.Infer<typeof Button>>(
+        {} as { color: 'gray' | 'red'; size?: 'sm' | 'xl' }
+      );
+    });
+
+    it('should make variants required by default', () => {
+      expectType<true>(
+        {} as Pick<W.Infer<typeof Button>, 'color'> extends { color: string }
+          ? true
+          : false
+      );
+    });
+
+    it('should make variants with default value optional', () => {
+      expectType<Pick<W.Infer<typeof Button>, 'size'>>(
+        {} as { size?: 'sm' | 'xl' }
+      );
+    });
+
+    it('should accept tag props', () => {
+      type Props = W.Infer<typeof Button>;
+      expectType<true>(
+        {} as Props['onClick'] extends
+          | React.MouseEventHandler<HTMLButtonElement>
+          | undefined
+          ? true
+          : false
+      );
+    });
+
+    it("should be a W.Component<'button'>", () => {
+      expectType<
+        W.Component<
+          'button',
+          {
+            color: {
+              gray: string;
+              red: string;
+            };
+            size: {
+              sm: string;
+              xl: string;
+            };
+          },
+          {}
+        >
+      >(Button);
+    });
+  });
+
+  describe('when using custom component', () => {
+    interface PostProps {
+      className?: string;
+      post: { id: string };
+      isActive: string;
+    }
+
+    const mockPost = { id: '1' };
+    const Post: React.FC<PostProps> = ({ className }) => (
+      <article className={className}>Post</article>
+    );
+    const variants = {
+      isActive: (yes: boolean) => (yes ? 'yes' : 'no'),
+      color: {
+        gray: 'x',
+        red: 'y',
+      },
+    } as const;
+    const Custom = w(Post, {
+      className: defaultClassName,
+      variants,
+      defaultProps: {
+        isActive: false,
+      },
+    });
+
+    it('should render without errors', () => {
+      render(<Custom post={mockPost} color="gray" />);
+      expect(screen.getByRole('article')).toBeInTheDocument();
+    });
+
+    describe.each([
+      [
+        'color=gray',
+        { color: 'gray' },
+        [defaultClassName, variants.color.gray, variants.isActive(false)],
+      ],
+      [
+        'color=gray isActive=true',
+        { color: 'gray', isActive: true },
+        [defaultClassName, variants.color.gray, variants.isActive(true)],
+      ],
+      [
+        'color=red isActive=false',
+        { color: 'red', isActive: false },
+        [defaultClassName, variants.color.red, variants.isActive(false)],
+      ],
+    ])('if rendering with props %s', (_, props, expectedClassNames) => {
+      it('should pass the evaluated className prop', () => {
+        render(<Custom post={mockPost} {...(props as any)} />);
+        const element = screen.getByRole('article');
+        expect(element.className.split(' ').sort()).toEqual(
+          expectedClassNames.sort()
+        );
+      });
+    });
+
+    it('should set correct prop types inferred by variants and component props', () => {
+      expectType<W.Infer<typeof Custom>>(
+        {} as {
+          color: 'gray' | 'red';
+          isActive?: boolean;
+          post: { id: string };
+        }
+      );
+    });
+
+    it('should make variants required by default', () => {
+      expectType<true>(
+        {} as Pick<W.Infer<typeof Custom>, 'color'> extends { color: string }
+          ? true
+          : false
+      );
+    });
+
+    it('should make variants with default value optional', () => {
+      expectType<Pick<W.Infer<typeof Custom>, 'isActive'>>(
+        {} as { isActive?: boolean }
+      );
+    });
+
+    it('should keep custom component props', () => {
+      type Props = W.Infer<typeof Custom>;
+      expectType<true>(
+        {} as Props['post'] extends { id: string } ? true : false
+      );
+    });
+
+    it('should override component props if name match', () => {
+      type Props = W.Infer<typeof Custom>;
+      expectType<true>(
+        {} as Props['isActive'] extends boolean | undefined ? true : false
+      );
+    });
+
+    it("should be a W.Component<'button'>", () => {
+      expectType<
+        W.Component<
+          'button',
+          {
+            color: {
+              gray: string;
+              red: string;
+            };
+            size: {
+              sm: string;
+              xl: string;
+            };
+          },
+          {}
+        >
+      >(Custom);
+    });
+  });
+});
