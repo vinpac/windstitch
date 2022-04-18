56753de4f89c88b3db5377f8babbe8affbf0b920
Vinicius Pacheco <vin175pacheco@gmail.com>
2022-04-18 11:22:54 -0300

    feat(evaluateClassName): use defaultVariants values if prop is undefiend

--- src/utils.ts
+++ src/utils.ts
@@ -10,17 +10,20 @@ import { VariantsRecord } from './types';
 export const evaluateClassName = (
   props: Record<string, any>,
   variants: VariantsRecord,
+  defaultVariants?: Record<string, any>,
   defaultClassName = ''
 ): string => {
   const classNames = [defaultClassName, props.className || ''];
 
   Object.keys(variants).forEach(key => {
     const variant = variants[key];
+    const value =
+      props[key] === undefined ? defaultVariants?.[key] : props[key];
 
     if (typeof variant === 'function') {
-      classNames.push(variant(props[key], props, variants)?.trim());
+      classNames.push(variant(value, props, variants)?.trim());
     } else {
-      classNames.push(variant[props[key]]?.trim());
+      classNames.push(variant[value]?.trim());
     }
   });
 
